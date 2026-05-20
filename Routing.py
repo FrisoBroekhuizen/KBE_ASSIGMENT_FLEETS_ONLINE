95fc45f4fe31ddad9d7ce27a2e364af1b50ae958
Bas Oudejans <basoudejans@c-002-012-044.client.tudelft.eduvpn.nl>
2026-05-20 14:18:32 +0200

    Added simple routing function and example file using ORS and routingpy

--- Routing.py
+++ Routing.py
@@ -1,14 +1,21 @@
-# from _future_ import annotations
-
 import os
-from typing import List, Tuple, Optional
 
 from parapy.core import Base, Input, Attribute, Part, child, action
 from parapy.exchange import STEPWriter
 
-from machine import *
+from routingpy import ORS
 
 maindir = os.path.dirname(__file__)
 
-Location_A= ...
-Location_B= ...
+
+def ComputeRoute(start, end):
+    coordinates = [start, end]
+
+    client = ORS(api_key='eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImY0OThhNGEyMGQwYjRmZjE5MDdmOGU2NjQzMDY0ZGVjIiwiaCI6Im11cm11cjY0In0=')
+
+    route = client.directions(locations=coordinates, profile='driving-hgv')
+
+    routeDuration = route.duration
+    routeDistance = route.distance
+
+    return routeDuration, routeDistance
--- RoutingTest.py
+++ RoutingTest.py
@@ -5,7 +5,7 @@ coords = [[4.37621, 52.00334], [4.43178, 51.93681]]
 
 client = ORS(api_key='eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImY0OThhNGEyMGQwYjRmZjE5MDdmOGU2NjQzMDY0ZGVjIiwiaCI6Im11cm11cjY0In0=')
 
-route = client.directions(locations=coords, profile='driving-car')
+route = client.directions(locations=coords, profile='driving-hgv')
 
 # pprint((route.geometry, route.duration, route.distance, route.raw))
 print("Time taken:" + str(route.duration) + " seconds")
