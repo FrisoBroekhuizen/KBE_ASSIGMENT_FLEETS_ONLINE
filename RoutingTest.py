2ffbf52484c34d219f24109cca3784e661f1e93c
boudejans <bas.oudejans@kpnmail.nl>
2026-05-20 14:07:53 +0200

    Initial routing test

---
+++ RoutingTest.py
@@ -0,0 +1,12 @@
+from routingpy import ORS
+from pprint import pprint
+
+coords = [[4.37621, 52.00334], [4.43178, 51.93681]]
+
+client = ORS(api_key='eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImY0OThhNGEyMGQwYjRmZjE5MDdmOGU2NjQzMDY0ZGVjIiwiaCI6Im11cm11cjY0In0=')
+
+route = client.directions(locations=coords, profile='driving-car')
+
+# pprint((route.geometry, route.duration, route.distance, route.raw))
+print("Time taken:" + str(route.duration) + " seconds")
+print("Route distance:" + str(route.distance) + " meters")
