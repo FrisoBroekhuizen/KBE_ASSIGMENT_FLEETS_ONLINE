1ba722388af1b4ca9ee69c84fecbaedfae0dc69e
boudejans <bas.oudejans@kpnmail.nl>
2026-05-26 16:57:06 +0200

    Integrated depot arrangement and visualization into main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from parapy.core import Base, Input, Attribute, Part, child, action
 from parapy.exchange import STEPWriter
 
 from machine import *
+from DepotArrangement import *
 
 import Routing
 from TrailerArrangement import Item, TrailerPackingVisualization
@@ -330,6 +331,68 @@ class Fleet(Base):
         machines: List["Machine"] = Input([])
         trailers: List["Trailer"] = Input([])
 
+        parking_gap = 0.6
+
+        non_attachable_tools: List[Machine] = Input([])
+        attachable_tools: List = Input([])
+
+        # Returns the machines list sorted based on the length of the vehicles
+        @Attribute
+        def sorted_machines(self):
+            for machine in self.machines:
+                if type(machine).__bases__[0].__name__ == "Tool" or type(machine).__name__ == "Tool":
+                    if machine.vehicle_attachable == False:
+                        self.machines.remove(machine)
+                        self.non_attachable_tools.append(machine)
+                    else:
+                        self.attachable_tools.append([machine])
+            self.attachable_tools = sorted(self.attachable_tools, key=lambda m: m[0].overall_dimensions[0])
+            return sorted(self.machines, key=lambda m: m.overall_dimensions[0])
+
+        @Attribute
+        def non_attachable_tools_storage(self):
+            area = 0
+            for tool in self.non_attachable_tools:
+                area += tool.overall_dimensions[0] * tool.overall_dimensions[1]
+
+            return area
+
+        # Returns a list of positions of the vehicles in the depot, using the sorted machines list and a path width dependent on the turning radius of the largest vehicle in the row.
+        @Attribute
+        def machine_positions(self):
+            row_width = 0
+            positions = []
+            max_vehicle_length = max(m.overall_dimensions[0] for m in self.machines)
+            row_height = max_vehicle_length
+            longest_vehicle = None
+            attachable_tool_index = 0
+            for i, vehicle in enumerate(self.sorted_machines):
+                if row_width + vehicle.overall_dimensions[1] + self.parking_gap < self.overall_dimensions[1]:
+                    # If the machine is a tool, it should be added to the attachable_tools list to create the ghost vehicle
+                    if type(vehicle).__bases__[0].__name__ == "Tool" or type(vehicle).__name__ == "Tool":
+                        positions.append([row_height - max_vehicle_length, row_width])
+                        self.attachable_tools[attachable_tool_index].append(
+                            row_height - (max_vehicle_length - vehicle.overall_dimensions[0]))
+                        self.attachable_tools[attachable_tool_index].append(row_width)
+                        attachable_tool_index += 1
+                        row_width += vehicle.overall_dimensions[1] + self.parking_gap
+                        continue
+                    else:
+                        positions.append([row_height - vehicle.overall_dimensions[0], row_width])
+                    row_width += vehicle.overall_dimensions[1] + self.parking_gap
+                    # The longest vehicle is kept track of which is to be used to determine the path width
+                    if longest_vehicle == None:
+                        longest_vehicle = vehicle
+                    elif longest_vehicle.overall_dimensions[0] < vehicle.overall_dimensions[0]:
+                        longest_vehicle = vehicle
+                else:
+                    row_width = 0
+                    path_width = DeterminePathWidth(self, longest_vehicle)
+                    row_height += path_width + max_vehicle_length
+                    positions.append([row_height - vehicle.overall_dimensions[0], row_width])
+                    row_width += vehicle.overall_dimensions[1] + self.parking_gap
+            return positions
+
         # ------------------------------------------------------------------ #
         # Helper: distance in meters between two GPS points
         # ------------------------------------------------------------------ #
@@ -403,6 +466,46 @@ class Fleet(Base):
             """Machines and trailers currently stored in this depot."""
             return [*self.machines, *self.trailers]
 
+        @Part
+        def Floor(self):
+            return Box(width=self.overall_dimensions[0],
+                       length=self.overall_dimensions[1],
+                       height=0.1,
+                       position=translate(self.position, 'z', -0.1),
+                       color="Black")
+
+        @Part
+        def NonAttachableToolStorage(self):
+            return Box(width=np.sqrt(self.non_attachable_tools_storage),
+                       length=np.sqrt(self.non_attachable_tools_storage),
+                       height=0.1,
+                       position=translate(self.position, 'x',
+                                          self.overall_dimensions[0] - np.sqrt(self.non_attachable_tools_storage), 'y',
+                                          self.overall_dimensions[1] - np.sqrt(self.non_attachable_tools_storage)),
+                       color="Green")
+
+        @Part
+        def PlaceMachines(self):
+            return Box(quantify=len(self.sorted_machines),
+                       width=self.sorted_machines[child.index].overall_dimensions[0],
+                       length=self.sorted_machines[child.index].overall_dimensions[1],
+                       height=self.sorted_machines[child.index].overall_dimensions[2],
+                       position=translate(self.position, 'x', self.machine_positions[child.index][0], 'y',
+                                          self.machine_positions[child.index][1]),
+                       color=("Blue" if type(self.sorted_machines[child.index]).__bases__[0].__name__ == "Tool" or type(
+                           self.sorted_machines[child.index]).__name__ == "Tool"
+                              else "Yellow"))
+
+        @Part
+        def PlaceGhosts(self):
+            return Box(quantify=len(self.attachable_tools),
+                       width=max(m.overall_dimensions[0] for m in self.sorted_machines) -
+                             self.attachable_tools[child.index][0].overall_dimensions[0],
+                       length=self.attachable_tools[child.index][0].overall_dimensions[1],
+                       height=self.attachable_tools[child.index][0].overall_dimensions[2],
+                       position=translate(self.position, 'x', self.attachable_tools[child.index][1], 'y',
+                                          self.attachable_tools[child.index][2]),
+                       color="Black")
 
 class Trailer(Base):
     """
